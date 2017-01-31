343c7218f3dd6d39703244d188cfb79bbc2defaa
Helgi Steinarr <Helgi Steinarr>
2017-01-31 19:30:52 +0000

    Dæmi tvö og sett loopu utan um valmynd

--- git_forritunar_verkefni.py
+++ git_forritunar_verkefni.py
@@ -4,21 +4,29 @@ def daemi1():
     print("Tölurnar lagðar saman: ", tala1 + tala2)
     print("Tölurnar margfaldaðar: ", tala1 * tala2)
 
-print("-------------------")
-print("  [1] fyrir dæmi1  ")
-print("  [2] fyrir dæmi2  ")
-print("  [3] fyrir dæmi3  ")
-print("-------------------")
-val = int(input("Hvað velurðu?[1-3]: "))
-if val == 1:
-    print("Þú hefur valið að keyra dæmi 1.")
-    daemi1()
+def daemi2():
+    fornafn = str(input("Fornafn?: "))
+    eftirnafn = str(input("Eftirnafn?: "))
+    print("Halló", fornafn, eftirnafn)
 
-elif val == 2:
-    print("Þetta dæmi er ekki tilbúið.")
+valmynd = 1
+while(valmynd == 1):
+    print("\n-------------------")
+    print("  [1] fyrir dæmi1  ")
+    print("  [2] fyrir dæmi2  ")
+    print("  [3] fyrir dæmi3  ")
+    print("-------------------")
+    val = int(input("Hvað velurðu?[1-3]: "))
+    if val == 1:
+        print("Þú hefur valið að keyra dæmi 1.")
+        daemi1()
 
-elif val == 3:
-    print("Þetta dæmi er ekki tilbúið.")
+    elif val == 2:
+        print("Þú hefur valið að keyra dæmi 2.")
+        daemi2()
 
-else:
-    print("Ekki á milli 1-3..")
+    elif val == 3:
+        print("Þetta dæmi er ekki tilbúið.")
+
+    else:
+        print("Ekki á milli 1-3..")
